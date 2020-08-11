7232002bda2f5680025d157e5518ff87ee98ced5
Notorious <69278774+noto-rious@users.noreply.github.com>
2020-08-11 12:28:29 -0500

    Feature update.
    Added giveaway entry feature, added a server blacklist for this feature. Pretty terminal output update with better utf8 support.

--- NitroSelf.py
+++ NitroSelf.py
@@ -6,8 +6,9 @@ from os import system
 from colored import fg, bg, attr
 import logging
 import asyncio, json, time, traceback
+from random import randint
 
-app_version = 'v1.1.3'
+app_version = 'v1.2.0'
 
 if getattr(sys, 'frozen', False):
     application_path = os.path.dirname(sys.executable)
@@ -25,8 +26,10 @@ os.system('cls' if os.name == 'nt' else 'clear')
 
 if os == 'Windows':
     jfile = application_path + '\\token.json'
+    bfile = application_path + '\\svr_blacklist.txt'
 else:
     jfile = application_path + '/token.json'
+    bfile = application_path + '/svr_blacklist.txt'
 
 if os.path.exists(jfile):
     jdata = json.load(open(jfile))
@@ -36,7 +39,14 @@ else:
     jdata.close()
     json.load(open(jfile))
 
-#system('title Nitro.Self ' + app_version + ' - Developed by: Notorious')
+if os.path.exists(bfile):
+    blines = open(bfile).read().split('\n')
+else:
+    blines = open(bfile, 'w')
+    blines.write('')
+    blines.close()
+    blines = open(bfile).read().split('\n')
+
 print(f'\33]0;Nitro.Self ' + app_version + ' - Developed by: Notorious\a', end='', flush=True)
 
 triedC = []
@@ -47,6 +57,7 @@ color2 = fg('#BA98E4')
 color3 = fg('#FF0000')
 color4 = fg('#FFC813')
 color5 = fg('#335BFF')
+color6 = fg('#E0FFFF')
 res = attr('reset')
 
 print(color1 + " ███╗   ██╗██╗████████╗██████╗  ██████╗    ███████╗███████╗██╗     ███████╗")
@@ -70,6 +81,9 @@ if token == "Token_Here":
         time.sleep(8)
         sys.exit()
 
+blength = len(blines)-1
+print('[' + color5 + time.strftime('%I:%M:%S %p', time.localtime()).rstrip() + res + '][🔫] • Black-List Servers Loaded: [' + color2 + str(f"{blength:,d}") + res + ']')
+
 bot = commands.Bot(command_prefix='.', self_bot=True)
 ready = False
 
@@ -79,7 +93,7 @@ while True:
         async def on_message(message):
             global ready
             if not ready:
-                print('[' + color5 + time.strftime('%m/%d/%Y', time.localtime()).rstrip() + ' ' + time.strftime('%I:%M:%S %p', time.localtime()).rstrip() + res + '][' + color5 + '→' + res + '] - Listening for Nitro Codes in ' + color2 + str(len(bot.guilds)) + res + ' servers...')
+                print('[' + color5 + time.strftime('%I:%M:%S %p', time.localtime()).rstrip() + res + '][🔫] • Listening for Nitro Codes in [' + color2 + str(len(bot.guilds)) + res + '] servers...')
                 ready = True
             else:
                 found = 0
@@ -87,40 +101,52 @@ while True:
                 start_time = time.time()
                 if codeRegex.search(message.content):
                     codevariable = codeRegex.search(message.content).group(2)
-                    print('[' + color5 + time.strftime('%m/%d/%Y', time.localtime()).rstrip() + ' ' + time.strftime('%I:%M:%S %p', time.localtime()).rstrip() + res + '][' + color5 + '→' + res + '] - checking code : ' + color5 + codevariable + res)
+                    print('[' + color5 + time.strftime('%I:%M:%S %p', time.localtime()).rstrip() + res + '][🔫] • Checking code: ' + color5 + codevariable + res + ' • [' + color2 +  message.author.name + '#' + message.author.discriminator + res + ']')
                     if codevariable not in triedC:
                         if len(codevariable) == 16:
                             triedC.append(str(codevariable))
                             r = requests.post('https://discordapp.com/api/v6/entitlements/gift-codes/' + codevariable + '/redeem',
-                                headers={'authorization': token}).text
-                            #r = r.json()
+                                json={"channel_id": str(message.channel.id)}, headers={'authorization': token}).text
                         else:
                             delay = time.time() - start_time
-                            print('[' + color5 + time.strftime('%m/%d/%Y', time.localtime()).rstrip() + ' ' + time.strftime('%I:%M:%S %p', time.localtime()).rstrip() + res + '][' + color3 + 'x' + res + '] - fake code : ' + color3 + codevariable + res + ' - (Delay:' + color4 + ' %.3fs' % delay + res + ')')
+                            print('[' + color5 + time.strftime('%I:%M:%S %p', time.localtime()).rstrip() + res + '][📛] • Fake code: ' + color3 + codevariable + res + ' • [' + color2 +  message.author.name + '#' + message.author.discriminator.replace(' ', '')  + res + '] • (Delay:' + color4 + ' %.3fs' % delay + res + ')')
                     else:
                         delay = time.time() - start_time
-                        print('[' + color5 + time.strftime('%m/%d/%Y', time.localtime()).rstrip() + ' ' + time.strftime('%I:%M:%S %p', time.localtime()).rstrip() + res + '][' + color3 + '-' + res + '] - duplicate code : ' + color3 + codevariable + res + ' - (Delay:' + color4 + ' %.3fs' % delay + res + ')')
+                        print('[' + color5 + time.strftime('%I:%M:%S %p', time.localtime()).rstrip() + res + '][❗] • Duplicate code: ' + color3 + codevariable + res + ' • [' + color2 +  message.author.name + '#' + message.author.discriminator.replace(' ', '')  + res + '] • (Delay:' + color4 + ' %.3fs' % delay + res + ')')
                         pass
                     if 'nitro' in str(r):
                         delay = time.time() - start_time
-                        print('[' + color5 + time.strftime('%m/%d/%Y', time.localtime()).rstrip() + ' ' + time.strftime('%I:%M:%S %p', time.localtime()).rstrip() + res + '][' + color1 + '+' + res + '] - redeemed nitro : ' + color1 + codevariable + res + ' - (Delay:' + color4 + ' %.3fs' % delay + res + ')')
+                        print('[' + color5 + time.strftime('%I:%M:%S %p', time.localtime()).rstrip() + res + '][🎉] • Nitro Redeemed: ' + color1 + codevariable + res + ' • [' + color2 +  message.author.name + '#' + message.author.discriminator.replace(' ', '')  + res + ']')
                         found += 1
                         print(f'\33]0;Nitro.Self ' + app_version + ' - Developed by: Notorious - Nitro Redeemed: ' + str(found), end='', flush=True)
                     elif 'This gift has been redeemed already.' in str(r):
                         delay = time.time() - start_time
-                        print('[' + color5 + time.strftime('%m/%d/%Y', time.localtime()).rstrip() + ' ' + time.strftime('%I:%M:%S %p', time.localtime()).rstrip() + res + '][' + color3 + '-' + res + '] - already claimed : ' + color3 + codevariable + res + ' - (Delay:' + color4 + ' %.3fs' % delay + res + ')')
+                        print('[' + color5 + time.strftime('%I:%M:%S %p', time.localtime()).rstrip() + res + '][❗] • Code already claimed: ' + color3 + codevariable + res + ' • [' + color2 +  message.author.name + '#' + message.author.discriminator.replace(' ', '') + res + '] • (Delay:' + color4 + ' %.3fs' % delay + res + ')')
                     elif 'Unknown Gift Code' in str(r):
                         delay = time.time() - start_time
-                        print('[' + color5 + time.strftime('%m/%d/%Y', time.localtime()).rstrip() + ' ' + time.strftime('%I:%M:%S %p', time.localtime()).rstrip() + res + '][' + color3 + '-' + res + '] - invalid code : ' + color3 + codevariable + res + ' - (Delay:' + color4 + ' %.3fs' % delay + res + ')')
+                        print('[' + color5 + time.strftime('%I:%M:%S %p', time.localtime()).rstrip() + res + '][⛔] • Invalid code: ' + color3 + codevariable + res + ' • [' + color2 +  message.author.name + '#' + message.author.discriminator.replace(' ', '')  + res + '] • (Delay:' + color4 + ' %.3fs' % delay + res + ')')
                     elif str(r) == '':
                         pass
                     else:
                         print(r)
-
+                elif (('**giveaway**' in str(message.content).lower() or ('react with' in str(message.content).lower() and 'giveaway' in str(message.content).lower()))) and str(message.guild.id)  not in blines:
+                    try:
+                        await asyncio.sleep(randint(100, 200))
+                        await message.add_reaction("🎉")
+                        print('[' + color5 + time.strftime('%I:%M:%S %p', time.localtime()).rstrip() + res + '][✅] • Entered Giveaway • [' + color2 +  message.guild.name + ' > ' + message.channel.name + res + ']')
+                    except:
+                        print('[' + color5 + time.strftime('%I:%M:%S %p', time.localtime()).rstrip() + res + '][⛔] • Failed to enter Giveaway • [' + color2 +  message.guild.name + ' > ' + message.channel.name + res + ']')
+                elif '<@' + str(bot.user.id) + '>' in message.content and ('giveaway' in str(message.content).lower() or 'won' in str(message.content).lower() or 'winner' in str(message.content).lower()):
+                    print(color5 + time.strftime("%H:%M:%S ", time.localtime()) + res, end='')
+                    try:
+                        won = re.search("You won the \*\*(.*)\*\*", message.content).group(1)
+                    except:
+                        won = "UNKNOWN"
+                    print('[' + color5 + time.strftime('%I:%M:%S %p', time.localtime()).rstrip() + res + '][🎉] • Congratulations! You won Giveaway: ' + color6 + won + res + ' • [' + color2 +  message.guild.name + ' > ' + message.channel.name + res + ']')
 
         bot.run(token, bot=False)
     except:
-            file = open('Error-Log.txt', 'w')
+            file = open('Error_Log.txt', 'w')
             file.write(traceback.format_exc())
             file.close()
             exit(0)
